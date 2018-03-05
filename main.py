37e18a82bc010cd51a56e1adf1b41a5978e2d1cb
wla <w.lambertz@gmx.net>
2018-03-05 20:51:48 +0100

    LESSON 1

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 if __name__ == '__main__':
     str_hll_wrld = 'Hello World!'
+    int_val = 10
+    str_val = 'neues feld'
+    dbl_val = 10.
+    arr_val = ['a', 'b', 'c', 'd']
+    map_val = {'key': 'value'}
     print(str_hll_wrld)
